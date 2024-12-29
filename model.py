80657d1d1f32744f853fdcb89de18338eb92a16d
Sonia Kolay <sonia210804@gmail.com>
2024-12-29 21:59:48 +0530

    Update model.py

--- model.py
+++ model.py
@@ -21,23 +21,3 @@ def query_gemma2_model(prompt):
     except Exception as e:
         return f"Error: {str(e)}"
 
-# import requests
-# import json
-
-# template = {
-#     "model":"gemma2:2b",
-#     "prompt":"Why is the sky blue?",
-#     "stream": False
-# }
-
-# response = requests.post('http://127.0.0.1:11434/api/generate',json=template)
-
-# llm_response = json.loads(response.text)
-
-# print(response)
-
-# #print(response.text)
-
-# # print(llm_response.keys())
-
-# #print("Response by the google gemma 2 model -->",llm_response['response'])
